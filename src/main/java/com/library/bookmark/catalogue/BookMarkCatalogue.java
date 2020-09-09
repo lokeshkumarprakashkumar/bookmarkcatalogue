a4351221c7e614ffe5122910a6a50b3b85bfd64a
Lokesh Prakashkumar <lprakash@intension.de>
2020-09-09 20:25:03 +0200

    Initialized the project with dependencies

---
+++ src/main/java/com/library/bookmark/catalogue/BookMarkCatalogue.java
@@ -0,0 +1,18 @@
+package com.library.bookmark.catalogue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * BookMarkCatalogue is the bootstrap of the application.
+ * 
+ * @author lprakash
+ */
+@SpringBootApplication
+public class BookMarkCatalogue {
+	
+	public static void main(String[] args)
+    {
+        SpringApplication.run(BookMarkCatalogue.class, args);
+    }
+}
